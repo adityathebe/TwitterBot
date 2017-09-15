1b288e90815fed12f6b4ebbcc57444e2c2c80903
adityathebe <thebeaditya@gmail.com>
2017-09-15 09:25:39 +0545

    'first'

---
+++ config.js
@@ -0,0 +1,10 @@
+// This is our Configuration File
+// Keep It Secret !!
+
+module.exports = {
+    consumer_key:         '<YOUR_CONSUMER_KEY>',
+    consumer_secret:      '<YOUR_CONSUMER_SECRET_KEY>',
+    access_token:         '<YOUR_ACCESS_TOKEN>',
+    access_token_secret:  '<YOUR_ACCESS_TOKEN_SECRET>',
+    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests. 
+}
